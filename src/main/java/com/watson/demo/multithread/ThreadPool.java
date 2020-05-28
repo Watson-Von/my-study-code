b85363ec9a93bf8c4274b4a5377b68bc85c27096
watson_von <260482352@qq.com>
2020-05-28 14:17:48 +0800

    提交 ThreadPool 测试代码

--- src/main/java/com/watson/demo/multithread/ThreadPool.java
+++ src/main/java/com/watson/demo/multithread/ThreadPool.java
@@ -1,20 +1,55 @@
 package com.watson.demo.multithread;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 public class ThreadPool {
 
     public static void main(String[] args) {
 
-        ExecutorService executorService = Executors.newFixedThreadPool(1);
+        ExecutorService executorService = Executors.newFixedThreadPool(20);
 
         for (int i = 0; i < 10; i++) {
             executorService.submit(new MyThread(String.valueOf(i)));
         }
 
-        System.out.println("执行完了");
+        for (int i = 0; i < 10; i++) {
+            Future<User> submit = executorService.submit(new MyThread2());
+            try {
+                System.out.println("姓名:" + submit.get().getName() + ", 年龄:" + submit.get().getAge());
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
 
+        executorService.shutdown();
+        System.out.println("主线程执行完了");
+
+    }
+
+    static class MyThread2 implements Callable<User> {
+
+        @Override
+        public User call() {
+            return new User("不说", 10);
+        }
+    }
+
+    static class User {
+        private String name;
+        private int age;
+
+        User(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public String getName() {
+            return name;
+        }
     }
 
     static class MyThread implements Runnable {
@@ -27,11 +62,11 @@ public class ThreadPool {
 
         @Override
         public void run() {
-//            try {
-//                Thread.sleep(2000L);
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
+            try {
+                Thread.sleep(2000L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             System.out.println(threadName + ", 线程开始执行");
         }
     }
